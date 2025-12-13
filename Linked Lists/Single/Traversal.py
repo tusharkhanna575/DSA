90e88c4f7e7ac9029e7fe873404d91268f742c2e
Tushar Khanna <khanna.tushar5112@gmail.com>
2025-12-13 23:59:03 +0530

    updated

--- Linked Lists/Single/Traversal.py
+++ Linked Lists/Single/Traversal.py
@@ -1,8 +1,8 @@
 # Definition of singly linked list:
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
     def LLTraversal(self, head):
