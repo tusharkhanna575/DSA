0dae34443e9cadcaaa25b6e7d3d99df6ca96389e
Tushar Khanna <khanna.tushar5112@gmail.com>
2026-04-09 09:16:55 +0530

    format: improve spacing and readability in factorial method

--- Maths/Basic/Factorial.py
+++ Maths/Basic/Factorial.py
@@ -1,8 +1,8 @@
 class Solution:
     def factorial(self, n):
-        fact=1
+        fact = 1
         for i in range(2, n+1):
-            fact*=i
+            fact *= i
         return fact
 
 # OR
