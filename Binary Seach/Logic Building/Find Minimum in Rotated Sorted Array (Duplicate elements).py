e1814f2764cb8b3a8a3e0077e65cdffdb49ec7f9
Tushar Khanna <khanna.tushar5112@gmail.com>
2026-04-14 00:08:59 +0530

    refactor: update findMin method to improve duplicate handling and type annotations

--- Binary Seach/Logic Building/Find Minimum in Rotated Sorted Array (Duplicate elements).py
+++ Binary Seach/Logic Building/Find Minimum in Rotated Sorted Array (Duplicate elements).py
@@ -1,14 +1,19 @@
+from typing import List
+
+
 class Solution:
-    def findMin(self, arr):
+    def findMin(self, arr: List[int]) -> int:
         low = 0
         high = len(arr)-1
         ans = float('inf')
         while (low <= high):
             mid = (low+high)//2
             # duplicate check
-            if (arr[low] <= arr[high]):
-                ans = min(ans, arr[low])
-                break
+            if arr[low] == arr[mid] == arr[high]:
+                ans = min(ans, arr[mid])
+                low += 1
+                high -= 1
+                continue
             if (arr[low] <= arr[mid]):
                 ans = min(ans, arr[low])
                 low = mid+1
