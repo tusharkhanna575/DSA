6914700eccb75a7524d19991090494270677d05a
Tushar Khanna <khanna.tushar5112@gmail.com>
2026-04-14 09:18:01 +0530

    add new algorithms to find the smallest divisor and the Nth root of a number; update formatting in square root algorithm

---
+++ Binary Seach/On Answers/Find the Smallest divisor.py
@@ -0,0 +1,26 @@
+import math
+
+
+class Solution:
+    def smallestDivisor(self, nums, limit):
+
+        def help(arr, mid, n):
+            sum = 0
+            for i in range(n):
+                sum += math.ceil(arr[i]/mid)
+            return sum
+
+        n = len(nums)
+        if (n > limit):
+            return -1
+        low = 1
+        high = max(nums)
+        ans = -1
+        while (low <= high):
+            mid = (low+high)//2
+            if (help(nums, mid, n) <= limit):
+                ans = mid
+                high = mid-1
+            else:
+                low = mid+1
+        return ans
---
+++ Binary Seach/On Answers/Nth root of a Number.py
@@ -0,0 +1,25 @@
+class Solution:
+    def NthRoot(self, n, m):
+        
+        def help(mid, n, m):
+            ans = 1
+            for _ in range(1, n+1):
+                ans *= mid
+                if ans > m:
+                    return 2
+            if ans == m:
+                return 1
+            return 0
+        
+        low = 1
+        high = m
+        while (low <= high):
+            mid = (low+high)//2
+            mid_n = help(mid, n, m)
+            if mid_n == 1:
+                return mid
+            elif mid_n == 0:
+                low = mid+1
+            else:
+                high = mid-1
+        return -1
